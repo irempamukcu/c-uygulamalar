d1612e2822ca6dd3e1047ba98a84d3db9b3d5ae7
irempamukcu <73612106+irempamukcu@users.noreply.github.com>
2020-10-28 23:12:16 +0300

    Add files via upload

---
+++ ucakodeme.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+//ucakekodeme
+
+int main()
+{
+  int bagaj,el,asilbagaj,asilel,kgbasiodeme,odenecek;
+  bagaj = 50;
+  el = 41;
+  asilbagaj = 15;
+  asilel = 8;
+  kgbasiodeme = 8;
+  odenecek = ((bagaj - asilbagaj) + (el - asilel)) * kgbasiodemes;
+  printf("Bagaji %d, elindeki yuku %d olan kisi %d tl oder. ",bagaj,el,odenecek);
+  
+  return 0;
+  	
+	
+	
+	
+}
